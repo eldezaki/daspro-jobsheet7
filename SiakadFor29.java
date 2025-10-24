ea243d6289baff5299a19d20a83f440e887e634b
eldezaki <eldezaki>
2025-10-24 19:43:23 +0700

    commit 2

--- SiakadFor29.java
+++ SiakadFor29.java
@@ -3,17 +3,31 @@ public class SiakadFor29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        
+        int jmlLulus = 0, jmlTidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                jmlLulus++;
+            } else {
+                jmlTidakLulus++;
+            }
         }
+        
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+
+        System.out.println("Jumlah mahasiswa lulus: " + jmlLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jmlTidakLulus);
     }
 }
